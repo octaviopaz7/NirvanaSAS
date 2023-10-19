b44f99fe3639c0d5576609d7c1042217850f8db6
octaviopaz7 <octavioemanuelpaz@gmail.com>
2023-10-19 20:55:55 -0300

    modificado navBar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import { Link } from "react-scroll";
 import styles from "./NavBar.module.css";
+import {TiThMenu} from "react-icons/ti";
+import {AiFillCloseCircle} from "react-icons/ai";
 
 const NavBar = () => {
+
+  const [navBarOpen, setNavBarOpen] = useState(false);
+  const [windowDimension, setWindowDimension] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  }); 
+
+  const detecDimension = () => {
+    setWindowDimension({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize" , detecDimension);
+    windowDimension.width > 800 && setNavBarOpen(false);
+    return () => {
+    window.removeEventListener("resize", detecDimension);
+    };
+  },[windowDimension]);
+
   const links = [
     {
         id: 1,
@@ -31,14 +55,42 @@ const NavBar = () => {
   ];
 
   return (
-    <div className={styles.NavBar}>
-        <ul>
-        {links.map( x => (
-          <div>
-            <Link>{x.link === "MissionVision" ? "Mision Vision" : x.link}</Link>
-          </div>
-            ))}
-        </ul>
+    <div 
+      className={!navBarOpen ? styles.navBar : styles.navOpen}>
+        {!navBarOpen && <p className={styles.logo}> NirvanaSAS</p>}
+        {!navBarOpen && windowDimension.width < 800 ? (
+          <TiThMenu 
+          onClick={() => setNavBarOpen(!navBarOpen)} 
+          size={25} 
+          />
+        ) : (
+          windowDimension.width < 800 && (
+          <AiFillCloseCircle 
+          onClick={() => setNavBarOpen(!navBarOpen)} 
+          color="#f1f1f1" 
+          size={25} 
+          />
+          )  
+        )}
+        {navBarOpen || 
+          (windowDimension.width > 800 && (
+            <ul className={styles.linksContainer}>
+             {links.map((x) => (
+                <div>
+                  <Link 
+                    onClick={() => setNavBarOpen(false)}
+                    to={x.link}
+                    smooth
+                    duration={500}
+                    className={styles.navLink}
+                  > 
+                    {x.link === "MisionVision" ? "Mision Vision" : x.link}
+                  </Link>
+                  <div className={styles.border}></div>
+                </div>
+            ))} 
+          </ul>
+         ))}
     </div>
   );
 };
